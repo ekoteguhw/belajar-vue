61ad2155a0c010f62f42077a9385fa6e2f549667
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-03-27 00:53:46 +0700

    01 Intro to Vuejs: Event handling

--- 01-intro-to-vuejs/main.js
+++ 01-intro-to-vuejs/main.js
@@ -9,25 +9,56 @@ const app = new Vue({
         'Nyaman untuk semua gender',
       ],
       image: './assets/kaoskaki.jpg',
-      inventory: 50,
+      inventory: 20,
+      inStock: true,
       variants: [
         {
           id: 1,
-          color: 'gray',
+          color: 'Image 1',
+          image: './assets/one.jpg',
         },
         {
           id: 2,
-          color: 'blue',
+          color: 'Image 2',
+          image: './assets/two.jpg',
         },
         {
           id: 3,
-          color: 'brown',
+          color: 'Image 3',
+          image: './assets/three.jpg',
         },
         {
           id: 4,
-          color: 'black',
+          color: 'Image 4',
+          image: './assets/four.jpg',
         },
       ],
+    },
+    cart: 0,
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+      this.product.inventory -= 1;
+      if (this.product.inventory < 0) {
+        this.product.inStock = false;
+      }
+    },
+    checkStock: function() {
+      let stock = this.product.inventory;
+      if (stock > 5) {
+        this.product.inStock = true;
+        return 'available';
+      } else if (stock > 0 && stock <= 5) {
+        this.product.inStock = true;
+        return 'warning';
+      } else {
+        this.product.inStock = false;
+        return 'danger';
+      }
+    },
+    updateProduct: function(image) {
+      this.product.image = image;
     },
   },
 });
--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Belajar dari VueMastery
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/27hRU0E-ync" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
 * 01 Intro to Vue
   * Vue Instance
   * Attribute binding, shorthand for `v-bind` to `:`
   * Conditional rendering, using `v-if`, `v-if-else`, and `v-else`
   * List rendering, using `v-for`, and `:key`
+  * Event handling
