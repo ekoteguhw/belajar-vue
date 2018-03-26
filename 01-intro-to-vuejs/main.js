5e629f750e47287ddb5989463c4c6a63c9b18af5
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-03-26 22:26:56 +0700

    01 Intro to Vuejs: Conditional rendering

--- 01-intro-to-vuejs/main.js
+++ 01-intro-to-vuejs/main.js
@@ -3,8 +3,10 @@ const app = new Vue({
   data: {
     product: {
       title: 'Kaos kaki',
-      description: 'Kaos kaki yang nyaman dan enak.',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam iusto sit tempore, expedita, quis repellat at minima error quidem labore sint et rerum. Obcaecati architecto quia ipsa placeat suscipit a.',
       image: './assets/kaoskaki.jpg',
+      inventory: 50,
     },
   },
 });
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Belajar dari VueMastery
 * 01 Intro to Vue
   * Vue Instance
   * Attribute binding, shorthand for `v-bind` to `:`
+  * Conditional rendering, using `v-if`, `v-if-else`, and `v-else`
